c93a2a763ab527b1547c92375d87ec445cfa6b46
Michael Cheng <michaelcheng1991@gmail.com>
2018-01-30 19:19:31 -0800

    changed greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send({hi:'there'})
+  res.send({bye:'there'})
 })
 // if heroku didn't assign the port, use 5000
 const PORT = process.env.PORT || 5000    
